cd59c88956188d2a0a08405a04866ab597657a84
Rodrigo <suigh.rodrigo@gmail.com>
2024-09-11 11:43:56 -0300

    reacao_motora.py agora escrito somente com a lib gpiod

--- reacao_motora.py
+++ reacao_motora.py
@@ -1,35 +1,66 @@
-import caninos_sdk as k9
+import gpiod
 import time
 import random
+# Configurando o chip e as linhas GPIO
+# gpiochip{n} onde n vai de 0 a 4
+# A labrador possui 5 grupos de GPIO A, B, C, D, E como descrito na wiki
+# onde:
+# 0 = A
+# 1 = B
+# 2 = C
+# 3 = D
+# 4 = E
+# Para este exercicio vamos usar o grupo E e B que corresponde ao gpiochip4 e gpiochip1
 
-# Iniciando a Labrador
-labrador = k9.Labrador()
-# Iniciando o LED
-labrador.pin3.enable_gpio(k9.Pin.Direction.OUTPUT, alias="led")
-# Iniciando o botao
-labrador.pin7.enable_gpio(k9.Pin.Direction.INPUT, alias="button")
-# Iniciando o buzzer
-labrador.pin5.enable_gpio(k9.Pin.Direction.OUTPUT, alias="buzzer")
+chipE = gpiod.chip('/dev/gpiochip4')
+chipB = gpiod.chip('/dev/gpiochip1')
+led = chipE.get_line(3)  # pino 3
+button = chipE.get_line(2)  # pino 5
+buzzer = chipB.get_line(18)  # pino 7
+
+ACTIVE = 1
+INACTIVE = 0
+
+# Configurando as direçoes INPUT e OUTPUT
+INPUT = gpiod.line_request.DIRECTION_INPUT
+OUTPUT = gpiod.line_request.DIRECTION_OUTPUT
+
+# Configurando as requisiçoes
+config_led = gpiod.line_request()
+config_led.consumer = "led"
+config_led.request_type = OUTPUT # define a direção do pino como saída
+led.request(config_led)
+
+config_button = gpiod.line_request()
+config_button.consumer = "button"
+config_button.request_type = INPUT # define a direção do pino como entrada
+button.request(config_button)
+
+config_buzzer = gpiod.line_request()
+config_buzzer.consumer = "buzzer"
+config_buzzer.request_type = OUTPUT
+buzzer.request(config_buzzer)
+
+
+
 def apertar_botao():
-    tf = 0
     ti = time.time()
     while True:
-        if labrador.button.read()==1:
+        if button.get_value() == ACTIVE:
             tf = time.time()
-            labrador.led.low()
-            labrador.buzzer.low()
-            return tf-ti
+            led.set_value(INACTIVE)
+            buzzer.set_value(INACTIVE)
+            return tf - ti
 
-    
 def iniciar_exp():
     exp = input("Digite:\n1-para experimento motor visual;\n2-para experimento motor auditivo\n3-Sair\n")
     if exp == "1":
         resp = input("Pronto para iniciar?(y/n)")
         if resp == "y":
             print("Iniciando...")
-            tempo_espera = random.uniform(1,5)
+            tempo_espera = random.uniform(1, 5)
             time.sleep(tempo_espera)
-            labrador.led.high()
+            led.set_value(ACTIVE)
             tr = apertar_botao()
             print(f"Tempo de resposta: {tr:.4f} ms")
             iniciar_exp()
@@ -42,9 +73,9 @@ def iniciar_exp():
         resp2 = input("Pronto para iniciar?(y/n)")
         if resp2 == "y":
             print("Iniciando...")
-            t_espera = random.uniform(1,5)
+            t_espera = random.uniform(1, 5)
             time.sleep(t_espera)
-            labrador.buzzer.high()
+            buzzer.set_value(ACTIVE)
             tr = apertar_botao()
             print(f"Tempo de resposta: {tr:.4f} ms")
             iniciar_exp()
